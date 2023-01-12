6aa33b69b01720a7f338d87824f49db08cc53d2d
oleg <savinoleg654@gmail.com>
2023-01-12 17:32:28 +0300

    feat: create config.js file

--- src/js/model.js
+++ src/js/model.js
@@ -1,27 +1,28 @@
+import { API_URL } from "./config.js";
 export const state = {
-  recipe: {},
+	recipe: {},
 };
 
 export const loadRecipe = async function (id) {
-  try {
-    const response = await fetch(`https://forkify-api.herokuapp.com/api/v2/recipes/${id}`);
-    const data = await response.json();
+	try {
+		const response = await fetch(`${API_URL}/${id}`);
+		const data = await response.json();
 
-    if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+		if (!response.ok) throw new Error(`${data.message} (${response.status})`);
 
-    const { recipe } = data.data;
-    state.recipe = {
-      id: recipe.id,
-      title: recipe.title,
-      publisher: recipe.publisher,
-      sourceUrl: recipe.source_url,
-      image: recipe.image_url,
-      servings: recipe.servings,
-      cookingTime: recipe.cooking_time,
-      ingredients: recipe.ingredients,
-    };
-    console.log(state.recipe);
-  } catch (error) {
-    alert(error);
-  }
+		const { recipe } = data.data;
+		state.recipe = {
+			id: recipe.id,
+			title: recipe.title,
+			publisher: recipe.publisher,
+			sourceUrl: recipe.source_url,
+			image: recipe.image_url,
+			servings: recipe.servings,
+			cookingTime: recipe.cooking_time,
+			ingredients: recipe.ingredients,
+		};
+		console.log(state.recipe);
+	} catch (error) {
+		alert(error);
+	}
 };
--- src/js/views/recipeView.js
+++ src/js/views/recipeView.js
@@ -1,6 +1,7 @@
 import icons from 'url:../../img/icons.svg';
 import { Fraction } from 'fractional';
 
+
 class RecipeView {
 	#parentElement = document.querySelector('.recipe');
 	#data;
