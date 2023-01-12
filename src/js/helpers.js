49ac326d0c7593f0de289b3a87bb514ebf9b3d15
oleg <savinoleg654@gmail.com>
2023-01-12 17:52:15 +0300

    feat: implementation promise.race

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -1,6 +1,16 @@
+import { TIMEOUT_SECONDS } from "./config.js";
+
+const timeout = function (second) {
+	return new Promise(function (_, reject) {
+		setTimeout(function () {
+			reject(new Error(`Request took too long! Timeout after ${second} second`));
+		}, s * 1000);
+	});
+};
+
 export const getJson = async function(url){
 	try{
-		const response = await fetch(url);
+		const response = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)]) ;
 		const data = await response.json();
 
 		if (!response.ok) throw new Error(`${data.message} (${response.status})`);
