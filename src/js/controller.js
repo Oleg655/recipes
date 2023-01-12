02822ee02ffd5eb2cab71409b99f8c2b72f3a2ad
oleg <savinoleg654@gmail.com>
2023-01-12 19:15:21 +0300

    fix: refactor view

--- src/js/controller.js
+++ src/js/controller.js
@@ -8,21 +8,21 @@ import recipeView from './views/recipeView.js';
 
 
 const controlRecipes = async function () {
-  try {
-    const id = window.location.hash.slice(1);
+	try {
+		const id = window.location.hash.slice(1);
 
-    if (!id) return;
-    recipeView.renderSpinner();
+		if (!id) return;
+		recipeView.renderSpinner();
 
-    await model.loadRecipe(id);
+		await model.loadRecipe(id);
 
-    recipeView.render(model.state.recipe);
-  } catch (error) {
-		recipeView.renderError(`${error}`)
-  }
+		recipeView.render(model.state.recipe);
+	} catch (error) {
+		recipeView.renderError()
+	}
 };
 
-const init = function(){
+const init = function () {
 	recipeView.addHandlerRender(controlRecipes)
 }
 
