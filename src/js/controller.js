f316f3f38819032abba9959199d68080918bacb5
oleg <savinoleg654@gmail.com>
2023-01-15 18:54:32 +0300

    fix: refactor last changes

--- src/js/controller.js
+++ src/js/controller.js
@@ -1,7 +1,11 @@
 import * as model from './model.js';
 import recipeView from './views/recipeView.js';
 import searchView from './views/searchView.js';
+import resultsView from './views/resultsView.js';
 
+if(module.hot){
+	module.hot.accept()
+}
 
 const controlRecipes = async function () {
 	try {
@@ -18,16 +22,19 @@ const controlRecipes = async function () {
 	}
 };
 
-const controlSearchResults = async function(){
-	try{
+const controlSearchResults = async function () {
+	try {
+
+		resultsView.renderSpinner()
 
 		const query = searchView.getQuery()
 
-		if(!query) return
+		if (!query) return
 
 		await model.loadSearchResults(query)
-		console.log(model.state.search.results)
-	}catch(error){
+
+		resultsView.render(model.state.search.results)
+	} catch (error) {
 		console.log(error)
 	}
 }
