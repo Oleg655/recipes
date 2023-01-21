afb631a068def3de2fc8088c12a075a05ed176c9
oleg <savinoleg654@gmail.com>
2023-01-21 15:32:44 +0300

    feat: create previewView.js

--- src/js/views/bookmarksView.js
+++ src/js/views/bookmarksView.js
@@ -1,5 +1,6 @@
 import icons from 'url:../../img/icons.svg';
 import View from './view.js'
+import previewView from './previewView.js';
 
 class BookmarksView extends View {
 	_parentElement = document.querySelector('.bookmarks__list')
@@ -7,27 +8,10 @@ class BookmarksView extends View {
 	_message = ''
 
 	_generateMarkup() {
-		return this._data.map(this._generateMarkupPreview).join('')
-
+		return this._data.map(bookmark => previewView.render(bookmark, false)).join('')
 	}
 
-	_generateMarkupPreview(result) {
-		const id = window.location.hash.slice(1)
 
-		return `
-			<li class="preview">
-            <a class="preview__link ${result.id === id ? 'preview__link--active' : ''}" href="#${result.id} ">
-		<figure figure class="preview__fig" >
-			<img src="${result.image}" alt="${result.title}" />
-              </figure >
-	<div class="preview__data">
-		<h4 class="preview__title">${result.title}</h4>
-		<p class="preview__publisher">${result.publisher}</p>
-	</div>
-            </a >
-          </li >
-	`
-	}
 }
 
 export default new BookmarksView()
