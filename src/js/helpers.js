d3e096b38b7be16d1d0d022217de78b62df30934
oleg <savinoleg654@gmail.com>
2023-01-22 09:53:54 +0300

    fix: refactor request functions

--- src/js/helpers.js
+++ src/js/helpers.js
@@ -8,29 +8,16 @@ const timeout = function (second) {
 	});
 };
 
-export const getJson = async function (url) {
+export const AJAX = async function (url, uploadData = undefined) {
 	try {
-		const fetchData = fetch(url)
-		const response = await Promise.race([fetchData, timeout(TIMEOUT_SECONDS)]);
-		const data = await response.json();
-
-		if (!response.ok) throw new Error(`${data.message} (${response.status})`);
-
-		return data
-	} catch (error) {
-		throw error
-	}
-}
-
-export const sendJson = async function (url, uploadData) {
-	try {
-		const fetchData = fetch(url, {
+		const fetchData = uploadData ? fetch(url, {
 			method: 'POST',
 			headers: {
 				'Content-Type': 'application/json'
 			},
 			body: JSON.stringify(uploadData)
-		})
+		}) : fetch(url)
+
 		const response = await Promise.race([fetchData, timeout(TIMEOUT_SECONDS)]);
 		const data = await response.json();
 
@@ -40,6 +27,40 @@ export const sendJson = async function (url, uploadData) {
 	} catch (error) {
 		throw error
 	}
-
 }
+
+// export const getJson = async function (url) {
+// 	try {
+// 		const fetchData = fetch(url)
+// 		const response = await Promise.race([fetchData, timeout(TIMEOUT_SECONDS)]);
+// 		const data = await response.json();
+
+// 		if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+// 		return data
+// 	} catch (error) {
+// 		throw error
+// 	}
+// }
+
+// export const sendJson = async function (url, uploadData) {
+// 	try {
+// 		const fetchData = fetch(url, {
+// 			method: 'POST',
+// 			headers: {
+// 				'Content-Type': 'application/json'
+// 			},
+// 			body: JSON.stringify(uploadData)
+// 		})
+// 		const response = await Promise.race([fetchData, timeout(TIMEOUT_SECONDS)]);
+// 		const data = await response.json();
+
+// 		if (!response.ok) throw new Error(`${data.message} (${response.status})`);
+
+// 		return data
+// 	} catch (error) {
+// 		throw error
+// 	}
+
+// }
 
--- src/js/model.js
+++ src/js/model.js
@@ -1,5 +1,5 @@
 import { API_URL, RESULTS_PER_PAGE, KEY } from "./config.js";
-import { getJson, sendJson } from "./helpers.js";
+import { AJAX } from "./helpers.js";
 
 export const state = {
 	recipe: {},
@@ -29,7 +29,7 @@ const createRecipeObject = function (data) {
 
 export const loadRecipe = async function (id) {
 	try {
-		const data = await getJson(`${API_URL}${id}`)
+		const data = await AJAX(`${API_URL}${id}`)
 		state.recipe = createRecipeObject(data)
 
 		if (state.bookmarks.some(bookmark => bookmark.id === id))
@@ -44,7 +44,7 @@ export const loadRecipe = async function (id) {
 export const loadSearchResults = async function (query) {
 	try {
 		state.search.query = query
-		const data = await getJson(`${API_URL}?search=${query}`)
+		const data = await AJAX(`${API_URL}?search=${query}`)
 		console.log(data)
 
 		state.search.results = data.data.recipes.map(recipe => {
@@ -130,7 +130,7 @@ export const uploadRecipe = async function (newRecipe) {
 			servings: +newRecipe.servings,
 			ingredients,
 		}
-		const data = await sendJson(`${API_URL}?key=${KEY}`, recipe)
+		const data = await AJAX(`${API_URL}?key=${KEY}`, recipe)
 		state.recipe = createRecipeObject(data)
 		addBookmark(state.recipe)
 	} catch (error) {
